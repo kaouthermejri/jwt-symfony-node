0842333ee49d664db28fbcc13f7b7da54e903d48
Simon Turquier <turquiersimon@hotmail.fr>
2018-01-08 14:32:21 +0100

    Add firebase/php-jwt package + display token on view

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
+use \Firebase\JWT\JWT;
 
 class HomeController
 {
@@ -13,9 +15,24 @@ class HomeController
 	/**
 	 * @Route("/")
 	 */
-	public function index(Environment $twig)
+	public function index(Environment $twig, UserInterface $user = null)
 	{
-		return new Response($twig->render('index.html.twig'));
+
+		if (!is_null($user)) {
+		
+			$token = [
+				'username'	=> $user->getUsername(),
+				'roles'		=> $user->getRoles(),
+			];
+
+			$token = JWT::encode($token, 'my_secret_password');
+		} else {
+			$token = '';
+		}
+
+		return new Response($twig->render('index.html.twig', [
+			'token'	=> $token,
+		]));
 	}
 
 }
