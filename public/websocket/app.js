e2a814c0ec273c8dfd16c8e2ab9108a1069b27b0
Simon Turquier <turquiersimon@hotmail.fr>
2018-01-11 11:02:57 +0100

    Finalize websocket

--- public/websocket/app.js
+++ public/websocket/app.js
@@ -2,9 +2,34 @@
 
 let users = document.querySelector("#users")
 
+let addUser = (user) => {
+	let li = document.createElement('li')
+		
+	li.innerText = user.username
+	li.id = 'user' + user.username
+
+	users.appendChild(li)
+}
+
 if (users) {
+	
 	let socket = io('http://localhost:3000')
+	
 	socket.on('connect', () => {
-		console.log('I am connected')
+		socket.emit('identify', {
+			token: users.dataset.token
+		})
+	})
+
+	socket.on('users.new', ({user}) => addUser(user))
+
+	socket.on('users', ({users}) => {
+		for (let u in users) {
+			addUser(users[u])
+		}
+	})
+
+	socket.on('users.leave', ({user}) => {
+		users.removeChild(document.querySelector('#user' + user.username))
 	})
 }
--- public/websocket/server.js
+++ public/websocket/server.js
@@ -1,7 +1,52 @@
 // Server side
 
-let io = require('socket.io')(3000)
+let io 	= require('socket.io')(3000)
+let jwt = require('jsonwebtoken')
+
+let users = [];
 
 io.on('connection', socket => {
-	console.log('User has connected')
+
+	let current_user = null
+
+	socket.on('identify', ({token}) => {
+		try {
+			let decoded_token = jwt.verify(token, 'my_secret_password', {
+				algorithms: ['HS256']
+			})
+			
+			current_user = {
+				username: 	decoded_token.username,
+				roles: 		decoded_token.roles,
+				count: 1
+			}
+
+			let user = users.find(u => u.username === current_user.username)
+			if (user) {
+				user.count++
+			} else {
+				users.push(current_user)
+				socket.broadcast.emit('users.new', {user: current_user})
+			}
+
+			socket.emit('users', {users})
+
+		} catch (exception) {
+			console.log(exception.message);
+		}	
+	})
+
+	socket.on('disconnect', () => {
+		if (current_user) {
+
+			let user = users.find(u => u.username === current_user.username)
+			if (user) {
+				user.count--
+				if (user.count === 0) {
+					users = users.filter(u => u.username !== current_user.username)
+					socket.broadcast.emit('users.leave', {user: current_user})
+				}
+			}
+		}
+	})
 })
